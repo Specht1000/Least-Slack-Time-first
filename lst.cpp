786dd3b7e9942f75378ebc9d044646f71394dd7b
Guilherme Specht <103284410+Specht1000@users.noreply.github.com>
2024-04-13 19:46:58 -0300

    Update lst.cpp

--- lst.cpp
+++ lst.cpp
@@ -1,8 +1,7 @@
-// Nome: Guilherme Martins Specht
-// Trabalho 1 da disciplina Sistemas de Tempo Real do Prof. Roland Teodorowitsch no qual consiste
-// em desenvolver o escalonador de tarefas Least Slack Time first (LST).
-// O input para este código é gerado a partir do comando ./lst < input.txt
-// Última atualização: 05/04/2024
+// Trabalho 1 da disciplina de Sistemas de Tempo Real.
+// Desenvolver o escalonador Least Slack Time fist (LST).
+// Autor: Guilherme Martins Specht
+// Última atualização: 12/04/2024
 
 #include <iostream>
 #include <vector>
@@ -11,7 +10,7 @@
 using namespace std;
 
 struct Tarefa{
-    char letra;      // Letra correspondente a tarefa.
+    char letra;      // Letra correspondente à tarefa.
     int computacao;  // Tempo da computação.
     int periodo;     // Tempo do período.
     int deadline;    // Tempo de deadline.
@@ -22,8 +21,8 @@ struct Tarefa{
 int tempo_slack(const Tarefa& Tarefa, int tempo_atual){
     int tempo_ate_deadline; // Tempo até o fim do deadline.
     int slack; // Tempo de slack.
-    tempo_ate_deadline = Tarefa.inicio + Tarefa.deadline - tempo_atual; // Calculo do tempo até o fim do deadline.
-    slack = tempo_ate_deadline - Tarefa.tempo_falta; // Calculo do slack.
+    tempo_ate_deadline = Tarefa.inicio + Tarefa.deadline - tempo_atual; // Cálculo do tempo até o fim do deadline.
+    slack = tempo_ate_deadline - Tarefa.tempo_falta; // Cálculo do slack.
     return slack;
 }
 
@@ -34,7 +33,7 @@ int main(){
     while(cin >> num_tarefas >> tempo_execucao && (num_tarefas != 0 || tempo_execucao != 0)){ // Pega a linha que descreve o número de tarefas e o tempo de execução.
         
         vector<Tarefa> Tarefas(num_tarefas); // Vetor que guarda as structs de todas as tarefas.
-        
+
         if(num_tarefas > 26 || num_tarefas < 1){ // Verifica que o numero de tarefas não ultrapasse 26.
             cout << "ERRO! número excede os limites de 1 <= X <= 26." << endl;
             break;
@@ -43,12 +42,12 @@ int main(){
         for(int i = 0; i < num_tarefas; i++){
             Tarefas[i].letra = 'A' + i; // Define a letra das tarefas, A = (65)ascii, B = (66)ascii, C = (67)ascii e assim por diante.
             cin >> Tarefas[i].computacao >> Tarefas[i].periodo >> Tarefas[i].deadline; // Pega a linha que descreve computação, período e deadline. 
-            
+
             if(Tarefas[i].computacao > 2048 || Tarefas[i].periodo > 2048 || Tarefas[i].deadline > 2048 || Tarefas[i].computacao < 1 || Tarefas[i].periodo < 1 || Tarefas[i].deadline < 1){ // Verifica que o numero de atributos não ultrapasse 2048.
                 cout << "ERRO! número excede os limites de 1 <= X <= 2048." << endl;
                 break;
             }
-            
+
             Tarefas[i].tempo_falta = Tarefas[i].computacao; // Inicializa o tempo que falta para terminar uma computação.
             Tarefas[i].inicio = 0; // Inicializa o tempo em que a tarefa começa.
         }
@@ -73,12 +72,16 @@ int main(){
             for(Tarefa& Tarefa : Tarefas){
                 if (Tarefa.tempo_falta > 0 && tempo_slack(Tarefa, tempo_atual) < min_slack){ // Verifica se ainda tem tempo de execução restante e se o slack da tarefa atual é menor do que o min_slack da tarefa anterior.
                     min_slack = tempo_slack(Tarefa, tempo_atual); // Atribui a min_slack o menos slack entre as tarefas.
-                    proxima_tarefa = &Tarefa; // next_tarefa aponta para a tarefa atual.
+                    proxima_tarefa = &Tarefa; // Proxima_tarefa aponta para a tarefa atual.
                 }
             }
 
             if(proxima_tarefa){
-                escalonamento += proxima_tarefa->letra; // A tarefa com menor slack é escalonada.
+                char exec_char = proxima_tarefa->letra;
+                if (tempo_atual >= proxima_tarefa->inicio + proxima_tarefa->deadline) { // Verifica se está executando fora do deadline no período atual
+                    exec_char = tolower(exec_char); // Converte a letra para minúscula se a tarefa executa fora do deadline.
+                }
+                escalonamento += exec_char; // A tarefa com menor slack é escalonada.
                 
                 if(ultima_letra_tarefa != proxima_tarefa->letra){ // Verifica se teve troca de contexto ou seja se a letra anterior não é a mesma da atual
                     trocas_contexto++; // Houve uma troca de contexto
@@ -101,7 +104,7 @@ int main(){
             else{
                 escalonamento += '.'; // Caso não tenho nenhuma tarefa sendo executada é idle.
                 if(ultima_letra_tarefa != '.'){ // Se a última letra da tarefa for do alfabeto e aconteceu antes do idle.
-                    trocas_contexto++;
+                    trocas_contexto++; 
                     preempcoes++;
                 }
                 ultima_letra_tarefa = '.';
